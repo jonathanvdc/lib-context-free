4d0e4c19decacc45dca3a4d60a35219b5372d3ab
jonathanvdc <jonathan.vdc@outlook.com>
2015-10-04 00:10:15 +0200

    Added some comments

--- libcontextfree/GraphvizHandler.fs
+++ libcontextfree/GraphvizHandler.fs
@@ -5,12 +5,17 @@ open System.IO
 open IOHelpers
 
 module GraphvizHandler =
+    /// Writes a label attribute with the given label text.
+    /// Label attributes are formatted as ` [label="<label>"];`.
     let writeLabel (writer : TextWriter) (label : string) : unit =
         writer.Write(" [label=\"")
         writer.Write(label)
         writer.Write("\"];")
         writer.WriteLine()
 
+    /// Writes an unlabeled edge from one node to another.
+    /// Nodes are identified by their indices. 
+    /// Edges are formatted as `<fromIndex> -> <toIndex>;`.
     let writeEdge (writer : TextWriter) (fromIndex : int) (toIndex : int) : unit =
         writer.Write(fromIndex)
         writer.Write(" -> ")
@@ -18,31 +23,49 @@ module GraphvizHandler =
         writer.Write(";")
         writer.WriteLine()
 
+    /// Writes four spaces of indentation.
     let writeIndentation (writer : TextWriter) : unit =
         writer.Write(String.replicate 4 " ")
 
+    /// Writes a graphviz representation for the given node.
+    /// The output's root node will has the start index,
+    /// and this function returns an index that is suitable for
+    /// use as its successor's node index.
     let rec writeGraphvizNode (writer : TextWriter) (startIndex : int) (node : ParseTree<string, string>) : int = 
+        // First, write some indentation and the node's index.
         writeIndentation writer
         writer.Write(startIndex)
         match node with
         | EpsilonLeaf              -> 
+            // Writes the epsilon leaf's label, which is "&epsilon;" here.
             writeLabel writer "&epsilon;"
             startIndex + 1
         | TerminalLeaf t           ->
+            // Writes the terminal leaf's label, which is a terminal.
             writeLabel writer t
             startIndex + 1
         | ProductionNode(n, items) ->
+            // Writes the node's label, which is a nonterminal.
             writeLabel writer n
             let foldNode index item =
+                // Writes a child node to the output stream.
                 let nextIndex = writeGraphvizNode writer index item
+                // Write some indentation and an edge definition to the text writer.
                 writeIndentation writer
                 writeEdge writer startIndex index
                 nextIndex
+            // Writes this production node's children.
+            // They are folded one by one using the folding function above.
             items |> List.fold foldNode (startIndex + 1)
 
+    /// Writes a graphviz file that represents the given parse tree to the given text writer.
     let writeGraphviz (writer : TextWriter) (node : ParseTree<string, string>) = 
+        // Write the dot header.
         writer.WriteLine("digraph PARSETREE {")
+        // Write some indentation, and make all nodes circles.
         writeIndentation writer
         writer.WriteLine("node [shape = circle];")
+        // Write the parse tree itself
         ignore(writeGraphvizNode writer 0 node)
+        // Closing brace
         writer.WriteLine("}")
