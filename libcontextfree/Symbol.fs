de2553a41717bcc6fc202d334d01f0e2856ec895
Mauris Van Hauwe <mauris.van.hauwe@gmail.com>
2015-10-04 16:04:56 +0200

    Define a Symbol type
    "A generic union type which combines two types representing nonterminal
    and terminal symbols."

---
+++ libcontextfree/Symbol.fs
@@ -0,0 +1,8 @@
+﻿namespace libcontextfree
+
+/// A generic union type which combines two types representing nonterminal and terminal symbols.
+type Symbol<'nt, 't> =
+    /// Defines a nonterminal symbol.
+    | Nonterminal of 'nt
+    /// Defines a terminal symbol.
+    | Terminal of 't
