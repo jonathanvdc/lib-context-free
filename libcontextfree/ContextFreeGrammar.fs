a82559ff5e498d202cee96b9696a8f43cbf09153
Mauris Van Hauwe <mauris.van.hauwe@gmail.com>
2015-10-05 00:58:09 +0200

    Add stubs for CFG/PDA definitions
    As of writing, we haven't really handled any efficient algorithms in
    class yet, so we should probably hold writing the rest of this stuff off
    until later.

---
+++ libcontextfree/ContextFreeGrammar.fs
@@ -0,0 +1,6 @@
+﻿namespace libcontextfree
+
+/// Defines a formal context-free grammar as a quadruple (V, T, P, S) of
+/// nonterminals, terminals, production rules, and the starting symbol.
+type ContextFreeGrammar<'nt, 't when 'nt : comparison and 't : comparison> =
+    | ContextFreeGrammar of Set<'nt> * Set<'t> * Set<ProductionRule<'nt, 't>> * 'nt
---
+++ libcontextfree/PushDownAutomaton.fs
@@ -0,0 +1,25 @@
+﻿namespace libcontextfree
+
+/// The type of the transition map δ in a PushDownAutomaton<'Q, 'Σ, 'Γ>.
+type Transition<'Q, 'Σ, 'Γ when 'Q : comparison and 'Σ : comparison and 'Γ : comparison> =
+    Map<'Q * 'Σ option * 'Γ, Set<'Q * 'Γ list>>
+
+/// Defines a formal pushdown automaton as a 7-tuple (Q, Σ, Γ, δ, q0, Z0, F), where:
+///
+///   * Q is the set of states.            : Set<'Q>
+///   * Σ is the set of input symbols.     : Set<'Σ>
+///   * Γ is the set of stack symbols.     : Set<'Γ>
+///   * δ is the transition map.           : Transition<'Q, 'Σ, 'Γ>
+///   * q0 is the initial state.           : 'Q
+///   * Z0 is the initial stack symbol.    : 'Γ
+///   * F is the set of accepting states.  : Set<'Q>
+///
+type PushDownAutomaton<'Q, 'Σ, 'Γ when 'Q : comparison and 'Σ : comparison and 'Γ : comparison> =
+    | PushDownAutomaton of Set<'Q> * Set<'Σ> * Set<'Γ> * Transition<'Q, 'Σ, 'Γ> * 'Q * 'Γ * Set<'Q>
+
+module PushDownAutomaton =
+    /// Return whether the PDA accepts the given string (via accepting states).
+    let accepts (pda : PushDownAutomaton<'Q, 'Σ, 'Γ>) (w : 'Σ list) : bool =
+        match pda with
+        | PushDownAutomaton (Q, Σ, Γ, δ, q0, Z0, F) ->
+            false   // TODO
