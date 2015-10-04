40a91315566e3301ab60930d64f3ed6d445b1d1a
Mauris Van Hauwe <mauris.van.hauwe@gmail.com>
2015-10-04 16:06:32 +0200

    Use more descriptive type variables and Symbol
    Also, some small changes nobody really cares about.

--- libcontextfree/ParseTree.fs
+++ libcontextfree/ParseTree.fs
@@ -1,44 +1,37 @@
 ﻿namespace libcontextfree
 
 /// Defines a parse tree type, based on a nonterminal and a terminal type.
-type ParseTree<'Nonterminal, 'Terminal> =
+type ParseTree<'nt, 't> =
     /// Defines a terminal leaf, which is a type of parse tree that 
     /// contains only a single terminal.
-    | TerminalLeaf of 'Terminal
+    | TerminalLeaf of 't
     /// Defines a production rule node, which is identified by a nonterminal and a 
     /// list of child nodes.
-    | ProductionNode of 'Nonterminal * ParseTree<'Nonterminal, 'Terminal> list
+    | ProductionNode of 'nt * ParseTree<'nt, 't> list
 
 module ParseTreeHelpers =
     /// Computes the yield of a parse tree, which is a list of terminals.
-    let rec treeYield (tree : ParseTree<'a, 'b>) : 'b list =
+    let rec treeYield (tree : ParseTree<'nt, 't>) : 't list =
         match tree with
-        | TerminalLeaf x        -> 
-            // The yield of a terminal is the terminal's string representation.
-            [x]
-        | ProductionNode(_, xs) -> 
+        | TerminalLeaf t -> [t]
+        | ProductionNode(_, children) -> 
             // The yield of a production rule node is the concatenation of
             // its children's yields.
-            xs |> List.map treeYield 
-               |> List.concat
+            children |> List.collect treeYield
 
     /// Gets a parse tree's "head", i.e. the terminal or nonterminal
     /// in the root node.
-    let treeHead (tree : ParseTree<'a, 'b>) : Either<'a, 'b> =
+    let treeHead (tree : ParseTree<'nt, 't>) : Symbol<'nt, 't> =
         match tree with
-        | TerminalLeaf x       ->
-            // The head of a terminal leaf is the terminal's string representation.
-            Right x
-        | ProductionNode(x, _) -> 
-            // The head of a production rule node is the associated nonterminal.
-            Left x
+        | TerminalLeaf t -> Terminal t
+        | ProductionNode(nt, _) -> Nonterminal nt
 
     /// Performs a single derivation: a single nonterminal node (selected from the node list by the `splitAt` function)
     /// is replaced by its children.
-    let derive splitAt (nodes : ParseTree<'a, 'b> list) : ParseTree<'a, 'b> list option =
+    let derive splitAt (nodes : ParseTree<'nt, 't> list) : ParseTree<'nt, 't> list option =
         // `getProductionNodeChildren` is a local helper function that extracts
         // node children from production rule nodes.
-        let getProductionNodeChildren (node : ParseTree<'a, 'b>) =
+        let getProductionNodeChildren (node : ParseTree<'nt, 't>) =
             // Checks if this node is a production rule node.
             // If so, return its items. Otherwise, return None.
             match node with
@@ -63,7 +56,7 @@ module ParseTreeHelpers =
     /// at a time until only a list of terminal/epsilon leaves remain. 
     /// Said nonterminal is selected by the given split-at function.
     /// The input node list is not included in the result list.
-    let rec derivation splitAt (nodes : ParseTree<'a, 'b> list) : ParseTree<'a, 'b> list list =
+    let rec derivation splitAt (nodes : ParseTree<'nt, 't> list) : ParseTree<'nt, 't> list list =
         match derive splitAt nodes with
         | None   -> []
         | Some x -> x :: derivation splitAt x
@@ -71,35 +64,34 @@ module ParseTreeHelpers =
     /// Gets the given tree's entire derivation 
     /// sequence, including the input tree itself. 
     /// Nonterminal nodes are replaced by the given split-at function.
-    let derivationSequence splitAt (tree : ParseTree<'a, 'b>) : ParseTree<'a, 'b> list list =
+    let derivationSequence splitAt (tree : ParseTree<'nt, 't>) : ParseTree<'nt, 't> list list =
         // The derivation sequence for this parse tree is obtained, 
         // and the parse tree is explicitly prepended.
         [tree] :: derivation splitAt [tree] 
 
     /// Gets the set of production rules that are used in this parse tree.
-    let rec productionRules (tree : ParseTree<'a, 'b>) : ProductionRule<'a, 'b> Set =
+    let rec productionRules (tree : ParseTree<'nt, 't>) : ProductionRule<'nt, 't> Set =
         match tree with
         | TerminalLeaf _ -> Set.empty
-        | ProductionNode(head, items) -> 
-            let body = items |> List.map (fun x -> match x with
-                                                   | TerminalLeaf y       -> Right y
-                                                   | ProductionNode(y, _) -> Left y)
+        | ProductionNode(head, children) -> 
+            let body = children |> List.map treeHead
             let rule = ProductionRule(head, body)
-            items |> List.fold (fun result item -> productionRules item |> Set.union result) (Set.singleton rule)
+            let childRules = children |> List.map productionRules
+            Set.add rule (Set.unionMany childRules)
 
     /// Gets a string representation for a parse tree's "head", 
     /// i.e. the terminal or nonterminal in the root node.
-    let showTreeHead<'a, 'b> : ParseTree<'a, 'b> -> string = 
+    let showTreeHead<'nt, 't> : ParseTree<'nt, 't> -> string = 
         treeHead >> string
 
     /// Get a string representation for the yield of a parse tree.
-    let showTreeYield<'a, 'b> : ParseTree<'a, 'b> -> string = 
+    let showTreeYield<'nt, 't> : ParseTree<'nt, 't> -> string = 
         treeYield >> string
 
     /// Gets a string representation for the given tree's entire derivation 
     /// sequence, including the input tree itself. 
     /// Nonterminal nodes are replaced by the given split-at function.
-    let showDerivationSequence splitAt (tree : ParseTree<'a, 'b>) : string =
+    let showDerivationSequence splitAt (tree : ParseTree<'nt, 't>) : string =
         // First, every step in the derivation sequence is converted to the
         // concatenation of its trees' head strings.
         // Then, the results of that operation are joined with the " => " separator.
@@ -108,15 +100,15 @@ module ParseTreeHelpers =
 
     /// Gets a string representation for the given tree's entire leftmost derivation 
     /// sequence, including the input tree itself. 
-    let showLeftmostDerivationSequence<'a, 'b> : ParseTree<'a, 'b> -> string = 
+    let showLeftmostDerivationSequence<'nt, 't> : ParseTree<'nt, 't> -> string = 
         showDerivationSequence ListHelpers.splitAtFirst
 
     /// Gets a string representation for the given tree's entire rightmost derivation 
     /// sequence, including the input tree itself. 
-    let showRightmostDerivationSequence<'a, 'b> : ParseTree<'a, 'b> -> string = 
+    let showRightmostDerivationSequence<'nt, 't> : ParseTree<'nt, 't> -> string = 
         showDerivationSequence ListHelpers.splitAtLast
 
     /// Gets a string representation of the set of production rules that are used in this parse tree.
-    let showProductionRules (tree : ParseTree<'a, 'b>) : string =
+    let showProductionRules (tree : ParseTree<'nt, 't>) : string =
         productionRules tree |> Seq.mapi (fun index item -> (string index) + ". " + (string item))
                              |> String.concat (System.Environment.NewLine)
--- libcontextfree/ProductionRule.fs
+++ libcontextfree/ProductionRule.fs
@@ -1,32 +1,22 @@
 ﻿namespace libcontextfree
 
-/// Defines a type that is either a value of the
-/// left-hand generic parameter or a value of
-/// the right-hand generic parameter.
-type Either<'a, 'b> =
-    | Left of 'a
-    | Right of 'b
-
-    override this.ToString() =
-        // Define the string representation of an either type
-        // as the string representation of its contents.
-        match this with
-        | Left  x -> x.ToString()
-        | Right x -> x.ToString()
-
 /// Defines a production rule.
-type ProductionRule<'Nonterminal, 'Terminal> =
+type ProductionRule<'nt, 't> =
     /// A production rule is a tuple of a nonterminal - the rule's head -
     /// and a list of nonterminals and terminals - the rule's body.
-    | ProductionRule of 'Nonterminal * Either<'Nonterminal, 'Terminal> list
+    | ProductionRule of 'nt * Symbol<'nt, 't> list
 
     override this.ToString() =
         match this with
         | ProductionRule(head, body) ->
-            // Production rules with empty bodies
-            // are epsilon-rules.
-            let bodyString = match body with
-                             | [] -> "ε"
-                             | _  -> body |> List.map string 
-                                          |> List.fold (+) ""
+            let showSymbol (sym : Symbol<'nt, 't>) : string =
+                match sym with | Nonterminal nt -> nt.ToString()
+                               | Terminal t -> t.ToString()
+            
+            let bodyString =
+                match body with
+                | [] -> "ε"
+                | _  -> body |> List.map showSymbol
+                             |> List.fold (+) ""
+
             head.ToString() + " -> " + bodyString
