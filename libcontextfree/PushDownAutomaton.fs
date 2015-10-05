19e095a8a60521cf4b2aa4fdd9af89df85790f96
Mauris Van Hauwe <mauris.van.hauwe@gmail.com>
2015-10-05 06:14:03 +0200

    Implement PDAs, maybe?
    This is untested, and probably slow, but it should work.

--- libcontextfree/PushDownAutomaton.fs
+++ libcontextfree/PushDownAutomaton.fs
@@ -17,9 +17,43 @@ type Transition<'Q, 'Σ, 'Γ when 'Q : comparison and 'Σ : comparison and 'Γ :
 type PushDownAutomaton<'Q, 'Σ, 'Γ when 'Q : comparison and 'Σ : comparison and 'Γ : comparison> =
     | PushDownAutomaton of Set<'Q> * Set<'Σ> * Set<'Γ> * Transition<'Q, 'Σ, 'Γ> * 'Q * 'Γ * Set<'Q>
 
+exception StackUnderflow
+
 module PushDownAutomaton =
-    /// Return whether the PDA accepts the given string (via accepting states).
-    let accepts (pda : PushDownAutomaton<'Q, 'Σ, 'Γ>) (w : 'Σ list) : bool =
+    /// Run a PDA non-deterministically, and return the final set of (state, stack) pairs.
+    let runPDA (pda : PushDownAutomaton<'Q, 'Σ, 'Γ>) (input : 'Σ list) : Set<'Q * 'Γ list> =
+        // A working state is an ECLOSE'd set of (state, stack) pairs, plus the current word.
+        // To advance a step, we munch off a letter and follow transitions, then ECLOSE again.
         match pda with
         | PushDownAutomaton (Q, Σ, Γ, δ, q0, Z0, F) ->
-            false   // TODO
+            let step (symbol : 'Σ option) (ps : Set<'Q * 'Γ list>) : Set<'Q * 'Γ list> =
+                ps |> Set.map (fun (q, stack) ->
+                    match stack with
+                    | [] -> raise StackUnderflow
+                    | X::β ->
+                        let r = Option.get (δ.TryFind (q, symbol, X))
+                        Set.map (fun (p, α) -> (p, α @ β)) r
+                ) |> Set.unionMany
+
+            let eclose (ps : Set<'Q * 'Γ list>) : Set<'Q * 'Γ list> =
+                step None ps
+
+            let rec go (word : 'Σ list) (ps : Set<'Q * 'Γ list>) : Set<'Q * 'Γ list> =
+                match word with
+                | [] -> ps
+                | (a::w) ->
+                    go w (step (Some a) ps) |> eclose
+
+            go input (Set.singleton (q0, [Z0]))
+
+    let reachesFinalState (pda : PushDownAutomaton<'Q, 'Σ, 'Γ>) (input : 'Σ list) : bool =
+        match pda with
+        | PushDownAutomaton (Q, Σ, Γ, δ, q0, Z0, F) ->
+            let result = runPDA pda input
+            Set.exists (fun (q, _) -> Set.contains q F) result
+
+    let reachesEmptyStack (pda : PushDownAutomaton<'Q, 'Σ, 'Γ>) (input : 'Σ list) : bool =
+        match pda with
+        | PushDownAutomaton (Q, Σ, Γ, δ, q0, Z0, F) ->
+            let result = runPDA pda input
+            Set.exists (fun (_, s) -> List.isEmpty s) result
