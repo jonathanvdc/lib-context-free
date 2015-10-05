a2af930b3ca0900034dc489119fbe8fcaf340cae
Mauris Van Hauwe <mauris.van.hauwe@gmail.com>
2015-10-05 12:19:48 +0200

    Actually, scratch that. Here's CFG-to-PDA.

--- libcontextfree/PushDownAutomaton.fs
+++ libcontextfree/PushDownAutomaton.fs
@@ -1,11 +1,11 @@
 ﻿namespace libcontextfree
 
-/// The type of the transition map δ in a PushDownAutomaton<'Q, 'Σ, 'Γ>.
+/// The type of the transition map δ in a PushdownAutomaton<'Q, 'Σ, 'Γ>.
 type Transition<'Q, 'Σ, 'Γ when 'Q : comparison and 'Σ : comparison and 'Γ : comparison> =
     Map<'Q * 'Σ option * 'Γ, Set<'Q * 'Γ list>>
 
-/// Defines a formal pushdown automaton as a 7-tuple (Q, Σ, Γ, δ, q0, Z0, F), where:
-///
+/// Defines a formal Pushdown automaton as a 7-tuple (Q, Σ, Γ, δ, q0, Z0, F), where:
+///P
 ///   * Q is the set of states.            : Set<'Q>
 ///   * Σ is the set of input symbols.     : Set<'Σ>
 ///   * Γ is the set of stack symbols.     : Set<'Γ>
@@ -14,46 +14,41 @@ type Transition<'Q, 'Σ, 'Γ when 'Q : comparison and 'Σ : comparison and 'Γ :
 ///   * Z0 is the initial stack symbol.    : 'Γ
 ///   * F is the set of accepting states.  : Set<'Q>
 ///
-type PushDownAutomaton<'Q, 'Σ, 'Γ when 'Q : comparison and 'Σ : comparison and 'Γ : comparison> =
-    | PushDownAutomaton of Set<'Q> * Set<'Σ> * Set<'Γ> * Transition<'Q, 'Σ, 'Γ> * 'Q * 'Γ * Set<'Q>
+/// However, Q, Σ, and Γ can be derived from the representation of δ as a set. As such,
+/// our PDAs only consist of (δ, q0, Z0, F).
+type PushdownAutomaton<'Q, 'Σ, 'Γ when 'Q : comparison and 'Σ : comparison and 'Γ : comparison> =
+    | PushdownAutomaton of Transition<'Q, 'Σ, 'Γ> * 'Q * 'Γ * Set<'Q>
+    
+    member pdf.Q : Set<'Q> =
+        match pdf with
+        | PushdownAutomaton (δ, q0, Z0, F) ->
+            let extractQ (KeyValue((q, _, _), ps)) =
+                Set.add q (Set.map fst ps)
+            δ |> Seq.map extractQ |> Set.unionMany
 
-exception StackUnderflow
+    member pdf.Σ : Set<'Σ> =
+        match pdf with
+        | PushdownAutomaton (δ, q0, Z0, F) ->
+            let extractΣ (KeyValue((_, sym, _), _)) = sym
+            δ |> Seq.choose extractΣ |> Set.ofSeq
 
-module PushDownAutomaton =
-    /// Run a PDA non-deterministically, and return the final set of (state, stack) pairs.
-    let runPDA (pda : PushDownAutomaton<'Q, 'Σ, 'Γ>) (input : 'Σ list) : Set<'Q * 'Γ list> =
-        // A working state is an ECLOSE'd set of (state, stack) pairs, plus the current word.
-        // To advance a step, we munch off a letter and follow transitions, then ECLOSE again.
-        match pda with
-        | PushDownAutomaton (Q, Σ, Γ, δ, q0, Z0, F) ->
-            let step (symbol : 'Σ option) (ps : Set<'Q * 'Γ list>) : Set<'Q * 'Γ list> =
-                ps |> Set.map (fun (q, stack) ->
-                    match stack with
-                    | [] -> raise StackUnderflow
-                    | X::β ->
-                        let r = Option.get (δ.TryFind (q, symbol, X))
-                        Set.map (fun (p, α) -> (p, α @ β)) r
-                ) |> Set.unionMany
+    member pdf.Γ : Set<'Γ> =
+        match pdf with
+        | PushdownAutomaton (δ, q0, Z0, F) ->
+            let extractΓ (KeyValue((_, _, γ), ps)) =
+                let γs = Seq.collect snd ps
+                Set.add γ (Set.ofSeq γs)
+            δ |> Seq.map extractΓ |> Set.unionMany
 
-            let eclose (ps : Set<'Q * 'Γ list>) : Set<'Q * 'Γ list> =
-                step None ps
-
-            let rec go (word : 'Σ list) (ps : Set<'Q * 'Γ list>) : Set<'Q * 'Γ list> =
-                match word with
-                | [] -> ps
-                | (a::w) ->
-                    go w (step (Some a) ps |> eclose)
-
-            go input (Set.singleton (q0, [Z0]) |> eclose)
-
-    let reachesFinalState (pda : PushDownAutomaton<'Q, 'Σ, 'Γ>) (input : 'Σ list) : bool =
-        match pda with
-        | PushDownAutomaton (Q, Σ, Γ, δ, q0, Z0, F) ->
-            let result = runPDA pda input
-            Set.exists (fun (q, _) -> Set.contains q F) result
-
-    let reachesEmptyStack (pda : PushDownAutomaton<'Q, 'Σ, 'Γ>) (input : 'Σ list) : bool =
-        match pda with
-        | PushDownAutomaton (Q, Σ, Γ, δ, q0, Z0, F) ->
-            let result = runPDA pda input
-            Set.exists (fun (_, s) -> List.isEmpty s) result
+module PushdownAutomaton =
+    /// Slide 75
+    let ofCFG = function
+        | ContextFreeGrammar (V, T, P, S) ->
+            let q = ()
+            let δ : Transition<unit, 't, Symbol<'nt, 't>> =
+                let δ1 = seq [for ProductionRule(A, β) in P ->
+                              ((q, None, Nonterminal A), Set.singleton (q, β))]
+                let δ2 = seq [for t in T ->
+                              ((q, Some t, Terminal t), Set.singleton (q, []))]
+                Map.ofSeq (Seq.append δ1 δ2)
+            PushdownAutomaton (δ, q, Nonterminal S, Set.empty)
