dce37669118c4ff0b4c0f4b31a3d7a2857b7d4fb
jonathanvdc <jonathan.vdc@outlook.com>
2015-10-10 16:09:20 +0200

    Rewrote Result.sequence to not evaluate everything

--- libcontextfree/Result.fs
+++ libcontextfree/Result.fs
@@ -6,6 +6,16 @@ type Result<'a> =
 | Error of string
 
 module Result =
+    /// Finds out if the given result value indicates success.
+    let isSuccess (x : Result<'a>) = 
+        match x with
+        | Success _ -> true
+        | _         -> false
+
+    /// Determines if the given result was unsuccessful.
+    let isError (x : Result<'a>) = 
+        not (isSuccess x)
+
     /// Chain computations that might fail.
     let bind (binder : 'a -> Result<'b>) (result : Result<'a>) : Result<'b> =
         match result with
@@ -18,11 +28,22 @@ module Result =
 
     /// Return a list of all given results, if they are all Successes; otherwise,
     /// return the first error in the list.
-    let rec sequence : Result<'a> list -> Result<'a list> =
-        function
-        | []              -> Success []
-        | Success r :: rs -> map (fun xs -> r :: xs) (sequence rs)
-        | Error e   :: _  -> Error e
+    let rec sequence (rs : #seq<Result<'a>>) : Result<'a list> =
+        let folder (state : Result<'a list>) (item : Result<'a>) =
+            match state with
+            | Error _    -> state
+            | Success xs -> map (fun x -> x :: xs) item
+
+        // This behaves just like Seq.fold, except that we're 
+        // early-outing if something goes wrong.
+        let folded = rs |> Seq.scan folder (Success [])
+                        |> Seq.cache
+        match Seq.tryFind isError folded with
+        | Some error -> error
+        | None ->
+            folded |> Seq.last
+                   |> map List.rev
+            
 
     /// Print an Error, or the result of applying a function to a Success value.
     /// Success and Error values are printed to stdout and stderr, respectively;
